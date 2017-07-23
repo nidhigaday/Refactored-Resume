a5e20c86584658c6fe0b46c67782eb39c29f4b75
Spacevash <nidhi.gaday@gmail.com>
2017-07-23 15:23:25 -0400

    removed mobile

--- js/helper.js
+++ js/helper.js
@@ -3,7 +3,7 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span class="sec_role">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+// var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text"><a href="https://ca.linkedin.com/in/nidhigaday" target="_blank" class="white-text">%data%</a></span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text"><a href="https://github.com/nidhigaday" target="_blank" class="white-text">%data%</a></span></li>';
